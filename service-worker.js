3239150bb5db81b44b9f643d904979a711123fe8
Chite-17 <chitenthemunthali@gmail.com>
2024-08-28 17:45:55 +0200

    Updated

--- service-worker.js
+++ service-worker.js
@@ -21,19 +21,12 @@ const CACHE = "pwabuilder-page";
 
 const offlineFallbackPage = "index.html";
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
       .then((cache) => cache.add(offlineFallbackPage))
   );
 });
-
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
