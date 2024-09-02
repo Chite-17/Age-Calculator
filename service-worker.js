58cc2de14eaf77cbaf01d14a6d1ccf0c222dbbca
Chite-17 <145149531+Chite-17@users.noreply.github.com>
2024-09-02 15:59:51 +0200

    New Update

--- service-worker.js
+++ service-worker.js
@@ -13,42 +13,50 @@ const urlsToCache = [
   '/background.jpg'
 ];
 
-// This is the "Offline page" service worker
+// const offlineFallbackPage = "index.html";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+   self.addEventListener("message", (event) => {
+     if (event.data && event.data.type === "SKIP_WAITING") {
+       self.skipWaiting();
+     }
+   });
 
-const CACHE = "pwabuilder-page";
+   self.addEventListener('install', function (event) {
+     event.waitUntil(
+       caches.open(CACHE_NAME)
+         .then(function(cache) {
+           console.log('Opened cache')
+         return cache.addAll(urlsToCache);
+          })
+     );
+   });
 
-const offlineFallbackPage = "index.html";
+   self.addEventListener('fetch', function(event) {
+        event.respondWith(
+           caches.match(event.request)
+             .then(function(response) {
+         if (response) {
+              return response;
+         }
+         return fetch(event.request).catch(function() {
+             return caches.match('/index.html');
+          });
+         })
+     );
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+   });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+   self.addEventListener('activate',function(event) {
+       var cacheWhitelist = [CACHE_NAME];
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+       event.waitUntil(
+         caches.keys().then(function(keyList) {
+           return Promise.all(keyList.map(function(key) {
+             if (cacheWhitelist.indexOf(key) === -1) {
+               return caches.delete(key);
+             }
+           }));
+        })
+       );
+   });This is the "Offline page" service worker
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
